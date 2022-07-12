81f9b05c86b5517020d9a497aa999565b5f5fb7d
Rajni Gujarati <78299612+rajnig567@users.noreply.github.com>
2022-07-12 10:16:36 +0530

    Delete getid.php

--- getid.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$data = array("data"=>array("is_ad"=>"true","banner"=>"banner ni id","native"=>"native ni id","interstitial"=>"interstitial ni id",
-,"app_open"=>"app open ni id"));
-echo json_encode($data);
-
-?>
